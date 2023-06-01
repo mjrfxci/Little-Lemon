faa8a756373d111dadce50c1b35f4403b528bee4
Miguel Romero <mjrf.xci@gmail.com>
2023-06-01 10:09:36 -0400

    changes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Routing from "./components/Routing";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <>
+      <Navbar />
+      <Routing />
+      <Footer />
+    </>
   );
 }
 
